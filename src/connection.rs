268b9fd36014851c4806d261044cac12916ddc90
Jaswanth Pinnepu <jaswanth.pinnepu8@gmail.com>
2026-03-15 15:02:27 -0400

    breaking; making changes towards frame parser

---
+++ src/connection.rs
@@ -0,0 +1,51 @@
+use std::error::Error;
+
+use bytes::{BufMut, BytesMut};
+use tokio::io::{AsyncReadExt, BufWriter};
+use tokio::net::TcpStream;
+
+use anyhow::Result;
+
+
+// connection instances for each connection
+struct Connection {
+    socket: BufWriter<TcpStream>,
+    buffer: BytesMut,
+}
+
+
+impl Connection {
+    pub fn new(socket: TcpStream) -> Self {
+        Self {
+            socket : BufWriter::new(socket),
+            // a 5kb buffer for each conection
+            buffer: BytesMut::with_capacity(5 * 1024),
+        }
+    }
+
+    pub async fn read_frame(&mut self) -> Result<Option<Frame>> {
+        // incrementally building the frame maybe?
+        loop {
+            if let Some(frame) = self.parse_frame()? {
+                return Ok(Some(frame))
+            }
+            // error varient returner while parsing has to treated as
+            // that the stream in closed or done
+            if 0 == self.socket.read_buf(&mut self.buffer).await? {
+                // return None, when clean connection reset
+                if self.buffer.is_empty() {
+                    return Ok(None)
+                } else {
+                    return Err("Connection reset into peer".into());
+                }
+            }
+        }
+    }
+
+    fn parse_frame(&mut self) -> Option<Frame> {
+        todo!("Define the parse frame and contiue")
+    }
+}
+
+
+
