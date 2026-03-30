397d988d8485acddb2a35972abbb9e2713651c5a
Jaswanth Pinnepu <jaswanth.pinnepu8@gmail.com>
2026-03-30 14:19:24 -0400

    added individual commands

---
+++ src/cmd/ping.rs
@@ -0,0 +1,32 @@
+use crate::{Connection, frame::Frame, parse::Parse, parse::ParseError};
+use anyhow::Result;
+
+use bytes::Bytes;
+
+#[derive(Debug, Default)]
+pub struct Ping {
+    msg: Option<Bytes>,
+}
+
+impl Ping {
+    pub fn new(msg: Option<Bytes>) -> Ping {
+        Ping { msg }
+    }
+
+    pub(crate) fn parse_frames(parse: &mut Parse) -> Result<Ping> {
+        match parse.next_bytes() {
+            Ok(msg) => Ok(Ping::new(Some(msg))),
+            Err(ParseError::EndOfStream) => Ok(Ping::default()),
+            Err(e) => Err(e.into()),
+        }
+    }
+
+    pub(crate) fn into_frame(self) -> Frame {
+        let mut frame = Frame::array();
+        frame.push_bulk(Bytes::from("ping".as_bytes()));
+        if let Some(msg) = self.msg {
+            frame.push_bulk(msg);
+        }
+        frame
+    }
+}
